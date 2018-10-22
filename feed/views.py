ed1753982c88d5c9188354ffcf41ef88a21c9db7
Chaeoh <limchyo>
2018-10-22 09:52:27 +0900

    final

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import Article, Comment, HashTag
 
 # Create your views here.
@@ -48,7 +49,32 @@ def index(request):
 
 def detail(request, article_id):
     article = Article.objects.get(id=article_id)
+    # article_list = Article.objects.all()
+    # comment_list = Comment.objects.filter(article__id=article_id)
+    # comment_list = article.article_comments.all()
+    hashtag_list = HashTag.objects.all()
+    # category_list = set([
+    #     (article.category, article.get_category_display())
+    #     for article in article_list
+    # ])
     ctx = {
-        "article" : article
+        "article" : article,
+        # "comment_list" : comment_list,
+        "hashtag_list" : hashtag_list,
+        # "category_list" : category_list,
     }
+
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        content = request.POST.get("content")
+        Comment.objects.create(
+            article=article,
+            username=username,
+            content=content,
+        )
+
+        return HttpResponseRedirect("/{}/".format(article_id))
+
     return render(request, "detail.html", ctx)
